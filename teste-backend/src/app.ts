cd1991d41ac8c6db7847509a2586cfa369c2d379
Gabriel Borges <gabrielborgesw2@gmail.com>
2025-01-26 23:22:41 -0300

    "ajuste app"

--- teste-backend/src/app.ts
+++ teste-backend/src/app.ts
@@ -1,20 +1,12 @@
 import express from "express";
-import path from "path";
-import cors from "cors";
 import cadastroRoutes from "./routes/cadastro.routes";
 import errorHandler from "./middlewares/errorHandler";
 import validateRequest from "./middlewares/validateRequest";
 
 const app = express();
 
-// Middleware para CORS (permitindo acesso do front-end)
-app.use(cors({ origin: "http://127.0.0.1:5500/src/view/" }));
-
 // Middleware para analisar JSON
 app.use(express.json());
-
-// Servir arquivos estáticos (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, "../view")));
 
 // Middleware de validação global
 app.use(validateRequest);
